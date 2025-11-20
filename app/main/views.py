190248bfeb2c1c159b099dcb90848e6a48c2a973
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-11-20 20:14:45 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,12 @@
 from flask import render_template
 from . import main
-
+from ..models import User  # Importante: Importar o modelo de Usuário
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    # Busca todos os usuários no banco de dados
+    # O order_by é opcional, mas ajuda a manter a lista organizada alfabeticamente
+    users = User.query.order_by(User.username).all()
+    
+    # Passa a lista de usuários para o template como a variável 'users'
+    return render_template('index.html', users=users)
