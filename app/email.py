20bb451688dc1175f048492045404dee187a34e4
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-11-20 21:02:39 -0300

    Update email.py

--- app/email.py
+++ app/email.py
@@ -2,38 +2,60 @@ from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
-
 import requests
-from datetime import datetime
-
+import json # Necessário para o SendGrid
 
 def send_async_email(app, msg):
     with app.app_context():
+        # Esta função é para SMTP (não usada na rota de API, mas mantida para compatibilidade)
         mail.send(msg)
-
-
-def send_email_zoho(to, subject, template, **kwargs):
-    app = current_app._get_current_object()
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
-    msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
-
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
-    print('Enviando mensagem (POST)...', flush=True)
-    print('URL: ' + str(app.config['API_URL']), flush=True)
-    print('api: ' + str(app.config['API_KEY']), flush=True)
-    print('from: ' + str(app.config['API_FROM']), flush=True)
-    print('to: ' + str(to), flush=True)
-    print('subject: ' + str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, flush=True)
-    #print('text: ' + "Novo usuário cadastrado: " + newUser, flush=True)
+    
+    # Renderiza o HTML do e-mail
+    html_body = render_template(template + '.html', **kwargs)
+    
+    # Monta o cabeçalho de autenticação
+    headers = {
+        "Authorization": "Bearer " + app.config['API_KEY'],
+        "Content-Type": "application/json"
+    }
+    
+    # Monta o payload (corpo) da requisição no formato estrito do SendGrid
+    data = {
+        "personalizations": [
+            {
+                "to": [
+                    {
+                        "email": to
+                    }
+                ],
+                "subject": app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject
+            }
+        ],
+        "from": {
+            "email": app.config['API_FROM']
+        },
+        "content": [
+            {
+                "type": "text/html",
+                "value": html_body
+            }
+        ]
+    }
 
-    resposta = requests.post(app.config['API_URL'], 
-                             auth=("api", app.config['API_KEY']), data={"from": app.config['API_FROM'], 
-                                                                        "to": to, 
-                                                                        "subject": app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, 
-                                                                        "html": render_template(template + '.html', **kwargs)})
+    # Envia a requisição POST para a API
+    print(f'Tentando enviar e-mail via SendGrid para {to}...', flush=True)
+    
+    response = requests.post(
+        app.config['API_URL'], 
+        headers=headers, 
+        json=data
+    )
+    
+    # Log para ajudar no debug (aparecerá no console do PythonAnywhere)
+    print(f'Status Code: {response.status_code}', flush=True)
+    print(f'Resposta: {response.text}', flush=True)
+
+    return response
